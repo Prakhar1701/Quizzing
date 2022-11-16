b9f7d846c3213490b69dce160936d739543f70b7
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2022-11-16 18:18:24 +0530

    Created And Modified Retrofit_Instance.java

---
+++ app/src/main/java/prakhar/developed/quizzing/Retrofit_Instance.java
@@ -0,0 +1,22 @@
+package prakhar.developed.quizzing;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class Retrofit_Instance {
+
+    private static Retrofit retrofit;
+    private static final String BASE_URL = "";
+
+    public static Retrofit getRetrofit() {
+
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder().
+                    baseUrl(BASE_URL).
+                    addConverterFactory(GsonConverterFactory.create()).
+                    build();
+        }
+
+        return retrofit;
+    }
+}
