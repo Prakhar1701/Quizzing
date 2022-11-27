ced543eb02df1696c8e0c7fed630e35002755af0
Prakhar <prakharagrawal170101@outlook.com>
2022-11-27 12:22:47 +0530

    Changing The Way Quiz Should Be Displayed
    Removed Recycler View
    
    Viewing One Quiz At A Time.

--- app/src/main/java/prakhar/developed/quizzing/API_Interface.java
+++ app/src/main/java/prakhar/developed/quizzing/API_Interface.java
@@ -1,11 +1,6 @@
 package prakhar.developed.quizzing;
 
-import java.util.List;
-
 import prakhar.developed.quizzing.model.MCQs;
-import prakhar.developed.quizzing.model.Options;
-import prakhar.developed.quizzing.model.Questions;
-import prakhar.developed.quizzing.model.Result;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
@@ -13,8 +8,4 @@ public interface API_Interface {
 
     @GET("?quiz=true")
     Call<MCQs> getMCQs();
-//    Call<List<Result>> getResult();
-//    Call<List<Questions>> getQuestion();
-//    Call<List<MCQs>> getOption();
-
 }
--- app/src/main/java/prakhar/developed/quizzing/MainActivity.java
+++ app/src/main/java/prakhar/developed/quizzing/MainActivity.java
@@ -1,27 +1,12 @@
 package prakhar.developed.quizzing;
 
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.ItemTouchHelper;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
-
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.Toast;
-
-import java.util.ArrayList;
 import java.util.List;
-
-import prakhar.developed.quizzing.API_Interface;
 import prakhar.developed.quizzing.model.MCQs;
-import prakhar.developed.quizzing.model.Options;
 import prakhar.developed.quizzing.model.Questions;
 import prakhar.developed.quizzing.model.Result;
-import prakhar.developed.quizzing.recyclerViewMCQ.RecyclerViewAdapter;
-import prakhar.developed.quizzing.recyclerViewMCQ.RecyclerViewModel;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -30,18 +15,13 @@ public class MainActivity extends AppCompatActivity {
 
     API_Interface apiInterface;
 
-    RecyclerView recyclerView;
-    LinearLayoutManager layoutManager;
-    List<RecyclerViewModel> MCQ;
-    RecyclerViewAdapter adapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         apiInterface = Retrofit_Instance.getRetrofit().create(API_Interface.class);
-        MCQ = new ArrayList<>();
+
 
 
         apiInterface.getMCQs().enqueue(new Callback<MCQs>() {
@@ -53,20 +33,9 @@ public class MainActivity extends AppCompatActivity {
                 MCQs MCQresponce = response.body();
 
                 Result result = MCQresponce.getResult();
-
-
                 List<Questions> question = result.getQuestions();
+//                List<Options> option = question.get(0).getOptions();
 
-                for (int i = 0; i < 6; i++) {
-                    String lable = question.get(i).getLable();
-                    List<Options> option = question.get(i).getOptions();
-
-                    MCQ.add(new RecyclerViewModel(lable, option.get(0).getLable(), option.get(1).getLable(), option.get(2).getLable(),  option.get(3).getLable()));
-
-
-                }
-                initRecyclerView();
-                Toast.makeText(MainActivity.this, String.valueOf(MCQ.size()), Toast.LENGTH_LONG).show();
             }
 
             @Override
@@ -76,20 +45,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
 //----------------------------------------------------------------------------------------------------------------------------
-
-//        initRecyclerView();   why not here ?
-
-
 //----------------------------------------------------------------------------------------------------------------------------
-    }
-
-    private void initRecyclerView() {
-        recyclerView = findViewById(R.id.recyclerView);
-        layoutManager = new LinearLayoutManager(this);
-        layoutManager.setOrientation(RecyclerView.VERTICAL);
-        recyclerView.setLayoutManager(layoutManager);
-        adapter = new RecyclerViewAdapter(MCQ,MainActivity.this);
-        recyclerView.setAdapter(adapter);
-        adapter.notifyDataSetChanged();
     }
 }
