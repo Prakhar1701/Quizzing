e2fac6e7a07e504a96f9f8e3b961867d436a37de
Prakhar <prakharagrawal170101@outlook.com>
2022-11-29 21:39:02 +0530

    Checkpoint Commit

--- app/src/main/java/prakhar/developed/quizzing/MainActivity.java
+++ app/src/main/java/prakhar/developed/quizzing/MainActivity.java
@@ -3,6 +3,7 @@ package prakhar.developed.quizzing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import java.util.List;
@@ -19,8 +20,9 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
-//    API_Interface apiInterface;
-//    int quizCount = 0;
+    API_Interface apiInterface;
+    int quizCount = 0;
+    int totalQuizCount = 6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +30,60 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        loadNewQuestion();
 
 //----------------------------------------------------------------------------------------------------------------------------
+        binding.buttonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadNewQuestion();
+            }
+        });
 //----------------------------------------------------------------------------------------------------------------------------
     }
 
+    String quizOutOf() {
+        String quizNumber = String.valueOf(quizCount + 1) + " / " + String.valueOf(totalQuizCount);
+        return quizNumber;
+    }
 
+    void loadNewQuestion() {
+        binding.textViewQuizNumber.setText(quizOutOf());
+//--------------------------------------------------------------------------------------------------
+        apiInterface = Retrofit_Instance.getRetrofit().create(API_Interface.class);
+        apiInterface.getMCQs().enqueue(new Callback<MCQs>() {
+
+            @Override
+            public void onResponse(Call<MCQs> call, Response<MCQs> response) {
+
+                MCQs MCQresponce = response.body();
+
+                Result result = MCQresponce.getResult();
+                List<Questions> question = result.getQuestions();
+
+                String lable = question.get(quizCount).getLable();
+                binding.textViewLable.setText(lable);
+
+                List<Options> option = question.get(quizCount).getOptions();
+
+                String optionOne = option.get(0).getLable();
+                String optionTwo = option.get(1).getLable();
+                String optionThree = option.get(2).getLable();
+                String optionFour = option.get(3).getLable();
+
+                binding.buttonOption1.setText(optionOne);
+                binding.buttonOption2.setText(optionTwo);
+                binding.buttonOption3.setText(optionThree);
+                binding.buttonOption4.setText(optionFour);
+
+                //-----------------------------------------------------------------------------------------
+                quizCount++;
+            }
+
+            @Override
+            public void onFailure(Call<MCQs> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Fail", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 }
